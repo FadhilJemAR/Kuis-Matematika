384c89db0e96addd6cac2af08aaedd48e21441e7
FadhilJemAR <fadhilar91128@gmail.com>
2026-02-28 09:22:16 +0800

    tambah jumlah soal

--- script.js
+++ script.js
@@ -25,34 +25,34 @@ const pertanyaan =  [
         pertanyaan:'7 x 5 + 9',
         jawaban:'44'
     },
-    // {
-    //     pertanyaan:'90 + 9 x 10',
-    //     jawaban:'180'
-    // },
-    // {
-    //     pertanyaan:'100 x 50 + 20',
-    //     jawaban:'5020'
-    // },
-    // {
-    //     pertanyaan:'Tentukan gradien dari garis lurus yang melewati (0,2) dan (3,4)',
-    //     jawaban:'2/3'    
-    // },
-    // {
-    //     pertanyaan:'Jika f(x) = 2x + 3 diketahui x = 10 maka f(10) = ?',
-    //     jawaban:'23'
-    // },
-    // {
-    //   pertanyaan:'14 / 2 + 7',
-    //   jawaban:'14'
-    // },
-    // {
-    //     pertanyaan:'8 x 9 / 4',
-    //     jawaban:'18'
-    // },
-    // {
-    //     pertanyaan:'80 / 8',
-    //     jawaban:'10'
-    // }
+    {
+        pertanyaan:'90 + 9 x 10',
+        jawaban:'180'
+    },
+    {
+        pertanyaan:'100 x 50 + 20',
+        jawaban:'5020'
+    },
+    {
+        pertanyaan:'Tentukan gradien dari garis lurus yang melewati (0,2) dan (3,4)',
+        jawaban:'2/3'    
+    },
+    {
+        pertanyaan:'Jika f(x) = 2x + 3 diketahui x = 10 maka f(10) = ?',
+        jawaban:'23'
+    },
+    {
+      pertanyaan:'14 / 2 + 7',
+      jawaban:'14'
+    },
+    {
+        pertanyaan:'8 x 9 / 4',
+        jawaban:'18'
+    },
+    {
+        pertanyaan:'80 / 8',
+        jawaban:'10'
+    }
   
 ]
 
